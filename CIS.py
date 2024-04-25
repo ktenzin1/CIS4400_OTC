108dbdd9f188c4af0b3156aaf3b69cc525005708
Karma Yangchen Tenzin <karmayangchentenzin@gmail.com>
2024-04-25 10:09:02 -0400

    Added python ingestion script

---
+++ CIS.py
@@ -0,0 +1,12 @@
+
+
+
+from google.colab import drive
+
+# Mount Google Drive
+drive.mount('/content/drive')
+
+# Specify the file path in Google Drive
+file_path = '/content/drive/My Drive/Data_Project.zip'
+
+# Use the file_path in your code to access the ZIP file
